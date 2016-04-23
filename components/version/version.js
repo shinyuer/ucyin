768ec463462847dc15f333bc2d077856bb7f70fc
shiny <35998585@qq.com>
2016-04-23 17:34:29 +0800

    a3

---
+++ components/version/version.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('myApp.version', [
+  'myApp.version.interpolate-filter',
+  'myApp.version.version-directive'
+])
+
+.value('version', '0.1');
+
+//<div>Angular seed app: v<span app-version></span></div>
